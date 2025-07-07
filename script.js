83ea4391117c039965be6a6860e8a141bb06e0b8
vammlesyeux <vamm.crdts@gmail.com>
2025-07-07 22:28:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function login() {
+  const username = document.getElementById("username").value.trim().toLowerCase();
+  const password = document.getElementById("password").value.trim();
+  const errorMsg = document.getElementById("error-msg");
+
+  if (username === "kinnette dainne".toLowerCase() && password === "July 6, 2003") {
+    document.getElementById("login-screen").style.display = "none";
+    document.getElementById("message-screen").style.display = "block";
+  } else {
+    errorMsg.textContent = "Incorrect username or password.";
+  }
+}
